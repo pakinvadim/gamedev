dd43771567362fe715ccb4d8980f76bd4bf2e35e
Vadim <Vadim@localhost>
2014-03-27 06:56:30 +0400

    ngh

--- BadCat/BadCat/RootSprite.h
+++ BadCat/BadCat/RootSprite.h
@@ -16,7 +16,7 @@
 }
 extern float const DoorAnimationDelay;
 
--(CGPoint) ConvertTouch:(CGPoint) point;
+@property CGPoint PositionScale;
 -(CCAnimation*) GetAnimation: (NSString*) likeName countFrame:(int) countFrame delay:(float) delay :(float) Wi :(float)Hi;
 -(CCAnimation*) GetAnimation: (NSString*) likeName arrayNumbersFrame:(NSMutableArray*) arrayNumbersFrame delay:(float) delay :(float)Wi:(float)Hi;
 
